f83d325beb6c2779d500255581f225aad681461e
Finlay Maguire <finlaymaguire@gmail.com>
2017-03-30 12:51:01 -0300

    Added SeqAn Dependency
    Configures the SeqAn Library Dependency with Cmake

--- src/amrtime.cpp
+++ src/amrtime.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include "AMRtimeConfig.h"
+#include <seqan/basic.h>
+#include <seqan/sequence.h>
+#include <seqan/stream.h>
+
 
 int main(int argc, char const ** argv){
     
     std::cout << "Version info:" << std::endl;
-    std::cout << AMRtime_VERSION_MAJOR 
-        << AMRtime_VERSION_MINOR 
-        << AMRtime_VERSION_PATCH 
-        << std::endl;
 
+    std::cout << AMRtime_VERSION_MAJOR << "."
+              << AMRtime_VERSION_MINOR << "." 
+              << AMRtime_VERSION_PATCH << "."
+              << std::endl;
 
+    std::cout << seqan::CharString("Test") << std::endl;
 
     return 0;
 }
