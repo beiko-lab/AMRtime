7c7781cdad487a2150a13e1bcddb8e0e53d6c597
Finlay Maguire <finlaymaguire@gmail.com>
2017-04-10 12:16:05 -0300

    Get basic CLI working with seqan

--- src/amrtime.cpp
+++ src/amrtime.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
+#include <seqan/arg_parse.h>
 #include "AMRtimeConfig.h"
-#include <seqan/basic.h>
-#include <seqan/sequence.h>
-#include <seqan/stream.h>
-
 
 int main(int argc, char const ** argv){
+
+    seqan::ArgumentParser parser("AMRtime");
+
+    setCategory(parser, "AMRtime");
+
+    setShortDescription(parser, "Rapid AMR prediction from metagenomic datasets");
+
+    setVersion(parser, AMRtime_VERSION);
+
+    addUsageLine(parser, "[\\fIOPTIONS\\fP] \\fIIN\fP");
+
+    addDescription(
+            parser,
+            "AMRtime is a tool to predict antimicrobial resistance (AMR) genes"
+            "present in metagenomic 2nd-generation sequencing datasets in a "
+            "rapid and reliable way.");
+
+    addArgument(parser, seqan::ArgParseArgument(
+        seqan::ArgParseArgument::INPUT_FILE, "IN"));
+  
+
+    seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
+
+    if (res != seqan::ArgumentParser::PARSE_OK){
+        return res == seqan::ArgumentParser::PARSE_ERROR;
+    }
     
-    std::cout << "Version info:" << std::endl;
-
-    std::cout << AMRtime_VERSION_MAJOR << "."
-              << AMRtime_VERSION_MINOR << "." 
-              << AMRtime_VERSION_PATCH << "."
-              << std::endl;
-
-    std::cout << seqan::CharString("Test") << std::endl;
+    seqan::CharString inputFile;
+    getArgumentValue(inputFile, parser, 0);
+    
+    std::cout << inputFile << std::endl;
 
     return 0;
 }
