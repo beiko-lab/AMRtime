cf4f82d13ddf8a39f7d68d34e475c91a03dac1ea
Finlay Maguire <finlaymaguire@gmail.com>
2017-08-04 22:19:48 -0300

    Add start of sam parsing

--- src/generate_training.cpp
+++ src/generate_training.cpp
@@ -11,10 +11,13 @@
 using namespace seqan; 
 
 int main(int argc, char *argv[]){
-    // generate_training genome1.fasta,genome2.fasta,genome3.fasta 4,2,3 genome1.gff,genome2.gff,genome3.gff
+    // generate_training genome1.fasta,genome2.fasta,genome3.fasta 4,2,3 genome1.gff,genome2.gff,genome3.gff coverageX read_length output_name
 
     std::vector<std::string> genome_list = split(argv[1], ',');
     std::vector<std::string> abundance_list = split(argv[2], ',');
+    std::uint32_t coverage_fold = atoi(argv[3]);
+    std::uint32_t read_length = atoi(argv[4]);
+    std::string output_name = argv[5];
 
     std::vector<uint32_t> abundances;
     for (uint32_t i=0; i<length(abundance_list); ++i) {
@@ -22,11 +25,37 @@ int main(int argc, char *argv[]){
         abundances.push_back(abundance);
     }
 
-    prepare_metagenome(genome_list, abundances);
+    std::string metagenome_fp = prepare_metagenome(genome_list, abundances, 
+                                                   output_name);
+
+    uint32_t read_number = estimate_read_depth(metagenome_fp,
+                                     coverage_fold,
+                                     read_length);
+
+    std::stringstream ss;
+    std::string simulated_sam_fp = output_name + ".sam"
+    ss << "mason_simulator -ir " << metagenome_fp << " -n " << read_number << " -oa " << simulated_sam_fp << " -o " << output_name + ".fq" << std::endl;
+    system(ss.c_str());
     
     std::vector<std::string> gff_list = split(argv[3], ',');
     std::vector<AMR_annotation> amr_annotations = read_amr_annotations(gff_list);
+    
+    create_labels(amr_annotations, simulated_sam_fp);
+
     return 0;
+}
+
+
+void create_labels(std::vector<AMR_annotation> annotations, std::string sam_fp){
+    
+    SamStream samStreamIn(sam_fp);
+    SamAlignmentRecord record;
+    while (!atEnd(samStreamIn)){
+        readRecord(record, samStreamIn);
+        // check annotations
+        // if in any range of simulated read print list of AROs to label file
+        // else print 0 on line
+    }
 }
 
 std::vector<AMR_annotation> read_amr_annotations(std::vector<std::string> gff_list) {
@@ -73,8 +102,9 @@ std::vector<AMR_annotation> read_amr_annotations(std::vector<std::string> gff_li
    return annotations;
 }
 
-int prepare_metagenome(std::vector<std::string> genome_list,
-                       std::vector<uint32_t> abundance_list) {
+std::string prepare_metagenome(std::vector<std::string> genome_list,
+                               std::vector<uint32_t> abundance_list,
+                               std::string output_name){
     // Copy the genomes up to necessary numbers into the artifical
     // metagenome contigs
     
@@ -106,19 +136,20 @@ int prepare_metagenome(std::vector<std::string> genome_list,
     }
     
     // dump artificial metagenome to single fasta file
+    std::string metagenome_fp = output_name + "metagenome.fasta";
     SeqFileOut seqFileOut;
-    if (!open(seqFileOut, "temp_metagenome.fasta")) {
+    if (!open(seqFileOut, metagenome_fp)) {
             std::cerr << "ERROR: Could not open file: temp_metagenome.fasta" << std::endl;
             return -1;
     }
 
     writeRecords(seqFileOut, ids, seqs);
 
-    return 0;
+    return metagenome_fp;
     
 }
     
-int count_nucleotides(std::string combined_genome_fp){
+uint32_t count_nucleotides(std::string combined_genome_fp){
     // read a fasta and count the nucleotides
     int nt_count = 0;
     
@@ -137,6 +168,24 @@ int count_nucleotides(std::string combined_genome_fp){
 
     return nt_count;
 }
+
+
+uint32_t estimate_read_depth(std::string combined_genome_fp,
+                             uint32_t coverage_fold,
+                             uint32_t read_length){
+    // Calculate the necessary read number for a specified read depth
+    
+    uint32_t nt_count = count_nucleotides(combined_genome_fp);
+    
+
+    double read_number_float = (coverage_fold * nt_count) / read_length;
+    
+    uint32_t read_number = atoi(read_number_float);
+
+    return read_number;
+
+}
+
 
 
 std::vector<std::string> split(std::string str, char delimiter) {
--- src/generate_training.h
+++ src/generate_training.h
@@ -16,8 +16,16 @@ std::vector<AMR_annotation> read_amr_annotations(std::vector<std::string> gff_li
 
 std::vector<std::string> split(std::string str, char delimiter);
 
-int prepare_metagenome(std::vector<std::string> genome_list, std::vector<uint32_t> abundance_list);
+std::string prepare_metagenome(std::vector<std::string> genome_list,
+                               std::vector<uint32_t> abundance_list,
+                               std::string output_name);
 
 int count_nucleotides(std::string fasta_fp); 
+
+void create_labels(std::vector<AMR_annotation> annotations, std::string sam_fp);
+
+uint32_t estimate_read_depth(std::string combined_genome_fp,
+                             uint32_t coverage_fold,
+                             uint32_t read_length);
 
 #endif // #ifndef GENERATE_TRAINING_H_
